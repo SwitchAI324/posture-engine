84e00d0694558a97dbc860b1da9c927faf487619
SwitchAI324 <andrew.h.goldberg@gmail.com>
2026-07-08 23:44:43 -0400

    Add files via upload

--- api/meeting.js
+++ api/meeting.js
@@ -407,7 +407,7 @@ $("join").addEventListener("click", function(){
           {
             on: "customer.speech.timeout",
             options: {
-              timeoutSeconds: 2,
+              timeoutSeconds: 4,
               triggerMaxCount: 2,
               triggerResetMode: "onUserSpeech"
             },
@@ -421,7 +421,7 @@ $("join").addEventListener("click", function(){
           {
             on: "customer.speech.timeout",
             options: {
-              timeoutSeconds: 4,
+              timeoutSeconds: 8,
               triggerMaxCount: 2,
               triggerResetMode: "onUserSpeech"
             },
